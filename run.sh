cf3e0160edb0025405c03e3d62faab6272d1b07d
Emil Cardell <emil.cardell@gmail.com>
2023-01-19 07:54:13 +0100

    La till start fil

---
+++ run.sh
@@ -0,0 +1,8 @@
+npm install
+npx nx build fam-stories-api
+npx nx build fam-stories-app
+mkdir -p dist/apps/fam-stories-api/public
+cp -r dist/apps/fam-stories-app/* dist/apps/fam-stories-api/public
+cd dist/apps/fam-stories-api/
+npm install
+node main.js
