7530c405f2eec9344e9e63944bb258c9cf4e5bf4
fannycardell <fannycardell@gmail.com>
2023-01-20 16:11:41 +0100

    changed back to npm install instead of npm ci

--- run.sh
+++ run.sh
@@ -1,8 +1,8 @@
-npm ci
+npm install
 npx nx build fam-stories-api
 npx nx build fam-stories-app
 mkdir -p dist/apps/fam-stories-api/public
 cp -r dist/apps/fam-stories-app/* dist/apps/fam-stories-api/public
 cd dist/apps/fam-stories-api/
-npm ci
+npm install
 node main.js
