50be2ad799c792eba91dacce3bb770452035af73
fannycardell <fannycardell@gmail.com>
2023-01-20 14:50:41 +0100

    trying npm ci

--- run.sh
+++ run.sh
@@ -1,8 +1,8 @@
-npm install
-#npx nx build fam-stories-api
-#npx nx build fam-stories-app
+npm ci
+npx nx build fam-stories-api
+npx nx build fam-stories-app
 mkdir -p dist/apps/fam-stories-api/public
 cp -r dist/apps/fam-stories-app/* dist/apps/fam-stories-api/public
 cd dist/apps/fam-stories-api/
-npm install
+npm ci
 node main.js
