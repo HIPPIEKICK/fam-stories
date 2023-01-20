d143224075558ba6b0267bdb358046c12e3e6313
fannycardell <fannycardell@gmail.com>
2023-01-20 14:34:27 +0100

    testing without npx nx build

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 npm install
-npx nx build fam-stories-api
-npx nx build fam-stories-app
+#npx nx build fam-stories-api
+#npx nx build fam-stories-app
 mkdir -p dist/apps/fam-stories-api/public
 cp -r dist/apps/fam-stories-app/* dist/apps/fam-stories-api/public
 cd dist/apps/fam-stories-api/
